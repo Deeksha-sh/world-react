a29f4cc760ff1f85168f94a86c1ee1218ffa78a0
Deeksha-sh <67338648+Deeksha-sh@users.noreply.github.com>
2021-01-26 12:25:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deeksha sharma - 12
+          Deeksha-12
         </p>
         <a
           className="App-link"
@@ -25,4 +25,4 @@ function App() {
 
 export default App;
 
-ok
+
